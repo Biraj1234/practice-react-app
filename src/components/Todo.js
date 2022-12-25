25b59975be2ab6e45659e342cc628179f136ab04
Biraj Shrestha <86864374+Biraj1234@users.noreply.github.com>
2022-12-25 12:59:07 +0545

    started react

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h2>Learn React Js</h2>
+      <div>
+        <button className="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  );
+}
+export default Todo;
